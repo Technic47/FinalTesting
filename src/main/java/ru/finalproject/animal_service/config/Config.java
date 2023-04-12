041815cb27ce32be97575c2c6408640dacd18e2b
Pavel <Technic47@gmail.com>
2023-04-12 14:29:49 +0300

    Start

---
+++ src/main/java/ru/finalproject/animal_service/config/Config.java
@@ -0,0 +1,83 @@
+package ru.finalproject.animal_service.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+@ComponentScan("ru.finalproject.animal_service")
+@EnableJpaRepositories("ru.finalproject.animal_service.repositories")
+@EntityScan("ru.finalproject.animal_service.models")
+@EnableWebMvc
+public class Config implements WebMvcConfigurer {
+    @Value("${datasource.driver}")
+    private String driver;
+    @Value("${spring.datasource.url}")
+    private String jdbcURL;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driver);
+        dataSource.setUrl(jdbcURL);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+//    @Bean
+//    public EntityManagerFactory entityManagerFactory() {
+//
+//        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+//        vendorAdapter.setGenerateDdl(true);
+//
+//        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+//        factory.setJpaVendorAdapter(vendorAdapter);
+//        factory.setPackagesToScan("ru.finalproject.animal_service.models");
+//        factory.setDataSource(dataSource());
+//        factory.afterPropertiesSet();
+//
+//        return factory.getObject();
+//    }
+
+//    @Bean
+//    public PlatformTransactionManager transactionManager() {
+//
+//        JpaTransactionManager txManager = new JpaTransactionManager();
+//        txManager.setEntityManagerFactory(entityManagerFactory());
+//        return txManager;
+//    }
+
+    @Bean(name = "multipartResolver")
+    public StandardServletMultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/");
+//        registry.addResourceHandler("/IMG/**")
+//                .addResourceLocations("classpath:/IMG/");
+//        registry.addResourceHandler("/preview/**")
+//                .addResourceLocations("classpath:/IMG/preview/");
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
+}
