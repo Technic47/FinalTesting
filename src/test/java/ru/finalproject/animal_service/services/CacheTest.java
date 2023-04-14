327e9852503cb98394324a1539986b5e852a3b14
Pavel <Technic47@gmail.com>
2023-04-14 19:26:15 +0300

    Cache tests started

---
+++ src/test/java/ru/finalproject/animal_service/services/CacheTest.java
@@ -0,0 +1,61 @@
+package ru.finalproject.animal_service.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addAnimalToCache() {
+    }
+
+    @Test
+    void delAnimalFromCache() {
+    }
+
+    @Test
+    void addFoodToCache() {
+    }
+
+    @Test
+    void delFoodFromCache() {
+    }
+
+    @Test
+    void addMovesToCache() {
+    }
+
+    @Test
+    void delMovesFromCache() {
+    }
+
+    @Test
+    void getAllAnimalsToShow() {
+    }
+
+    @Test
+    void convertToShow() {
+    }
+
+    @Test
+    void updateAnimalToShowList() {
+    }
+
+    @Test
+    void delAnimalFromShowList() {
+    }
+
+    @Test
+    void getAnimal() {
+    }
+
+    @Test
+    void getAnimalToShow() {
+    }
+}
